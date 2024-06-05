9fb5e0e963310b65f437ef7eeda4c731fe81c5d0
asoolito <roblox890king890@gmail.com>
2024-06-05 11:49:04 +0500

    %)Y@

---
+++ main.js
@@ -0,0 +1,5 @@
+let userName = prompt("ismingizni kiriting");
+let userID = prompt("space iDingizni kiriting");
+let userPassword = prompt("spce  Passwordingizni kiriting");
+let obj = {name: userName, password: userPassword,ID: userID}
+console.log(obj);
